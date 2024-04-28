012f59bc6e49f03199333ae3ac00d836b0b29f57
Abubaker Saeed <134003889+Abubaker3324@users.noreply.github.com>
2024-04-28 05:21:27 +0100

    Add files via upload

---
+++ Relations/Sol/data.pl
@@ -0,0 +1,102 @@
+:- module(data,[friend/2]).
+
+friend(ahmed, samy).
+friend(ahmed, fouad).
+friend(samy, mohammed).
+friend(samy, said).
+friend(samy, omar).
+friend(samy, abdullah).
+friend(fouad, abdullah).
+friend(abdullah, khaled).
+friend(abdullah, ibrahim).
+friend(abdullah, omar).
+friend(mostafa, marwan).
+friend(marwan, hassan).
+friend(hassan, ali).
+
+friend(hend, aisha).
+friend(hend, mariam).
+friend(hend, khadija).
+friend(huda, mariam).
+friend(huda, aisha).
+friend(huda, lamia).
+friend(mariam, hagar).
+friend(mariam, zainab).
+friend(aisha, zainab).
+friend(lamia, zainab).
+friend(zainab, rokaya).
+friend(zainab, eman).
+friend(eman, laila).
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
